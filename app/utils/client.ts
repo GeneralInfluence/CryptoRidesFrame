e15e30563a50c18ca5562280dc7760446828aff4
Jaxcoder <codenamejason@outlook.com>
2024-10-18 19:21:16 -0400

    fix client file

--- app/utils/client.ts
+++ app/utils/client.ts
@@ -5,16 +5,9 @@ import {
   http,
   parseEther,
 } from "viem";
-import { useWriteContract } from "wagmi";
-import {
-  type WriteContractErrorType,
-  type WriteContractReturnType,
-} from "@wagmi/core";
-// import { privateKeyToAccount } from "viem/accounts";
 import { base, baseSepolia } from "viem/chains";
 import { config } from "dotenv";
 import { contractConfig } from "@/app/utils/config";
-import { CryptoRidesNFTAbi } from "@/abis/CryptoRidesNFT";
 
 config();
 
@@ -27,49 +20,6 @@ export const publicClient = createPublicClient({
   chain: baseSepolia,
   transport: http(),
 });
-
-// const { writeContract, onError, onSuccess } = useWriteContract();
-
-// export const mintNFT = async function (
-//   userAddress: Address,
-//   tokenURI: string,
-//   handleTransactionSuccess: (txHash: string) => Promise<string | undefined>,
-//   handleTransactionError: (txHash: string) => Promise<string | undefined>
-// ) {
-//   const tx = writeContract(
-//     {
-//       address: contractConfig.contractAddress,
-//       abi: contractConfig.contractAbi,
-//       functionName: "safeMint",
-//       args: [userAddress, tokenURI],
-//     },
-//     {
-//       onSuccess: handleTransactionSuccess
-//     },
-//     {
-//       onError: handleTransactionError
-//     }
-//   );
-
-//   return tx;
-// };
-
-// JSON-RPC Account
-export const [address] = await walletClient.getAddresses();
-
-// export const sendMintTransaction = async (to: Address, uri: string) => {
-//   const { request } = await publicClient.simulateContract({
-//     account: address,
-//     address: contractConfig.contractAddress,
-//     abi: CryptoRidesNFTAbi,
-//     functionName: "safeMint",
-//     args: [to, uri],
-//   });
-
-//   const txn = await walletClient.writeContract(request);
-
-//   return txn;
-// };
 
 export const isWhitelisted = async (user: Address) => {
   const balance = await publicClient.readContract({
